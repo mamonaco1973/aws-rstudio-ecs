d80379f4ff8c4c4b863e5838fdb7cabe01a4612a
Mike Monaco <mamonaco1973@gmail.com>
2025-11-04 15:23:38 -0500

    v1 of ecs cluster

--- 04-ecs/security_groups.tf
+++ 04-ecs/security_groups.tf
@@ -53,6 +53,7 @@ resource "aws_security_group" "alb_service" {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 
   egress {
