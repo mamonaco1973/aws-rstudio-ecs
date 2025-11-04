05859279f5948e19e7d2e7108faba9a2f3a66397
Mike Monaco <mamonaco1973@gmail.com>
2025-11-04 13:59:29 -0500

    v1 of ecs cluster

--- 04-ecs/scripts/ecs_userdata.sh
+++ 04-ecs/scripts/ecs_userdata.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-echo "ECS_CLUSTER=${cluster_name}" > /etc/ecs/ecs.config
-systemctl enable --now ecs.service
+cat <<EOF > /etc/ecs/ecs.config
+ECS_CLUSTER=${cluster_name}
+ECS_ENABLE_CONTAINER_METADATA=true
+ECS_AWSVPC_BLOCK_IMDSV1=true
+EOF
+
+systemctl enable ecs.service
+systemctl start ecs.service
